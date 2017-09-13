25951c07b28336e8448df6396650d50be4147caa
GEROMAX <tobita0112@gmail.com>
2017-09-13 11:02:44 +0900

    cleanup issues_hepler_patch.

--- init.rb
+++ init.rb
@@ -1,10 +1,14 @@
 require_dependency 'redmine_subtask_list_accordion/hooks/subtask_list_accordion_hook'
-require_dependency 'redmine_subtask_list_accordion/patches/subtask_list_accordion_helper_patch'
+require_dependency 'redmine_subtask_list_accordion/patches/issues_helper_patch'
 require_dependency 'redmine_subtask_list_accordion/patches/user_preference_patch'
 
 ActionDispatch::Callbacks.to_prepare do
   unless UserPreference.included_modules.include?(RedmineSubtaskListAccordion::Patches::UserPreferencePatch)
     UserPreference.send :prepend, RedmineSubtaskListAccordion::Patches::UserPreferencePatch
+  end
+
+  unless IssuesHelper.included_modules.include?(RedmineSubtaskListAccordion::Patches::IssuesHelperPatch)
+    IssuesHelper.send :prepend, RedmineSubtaskListAccordion::Patches::IssuesHelperPatch
   end
 end
 
---
+++ lib/redmine_subtask_list_accordion/patches/issues_helper_patch.rb
@@ -0,0 +1,23 @@
+require_dependency("issues_helper")
+
+module RedmineSubtaskListAccordion
+  module Patches
+    module IssuesHelperPatch
+      extend ActiveSupport::Concern
+
+      # add method to IssuesHelper
+      def expand_tree_at_first?(issue, user)
+        if issue.descendants.visible.count <= user.pref.subtasks_default_expand_limit_upper
+          return true
+        else
+          return false
+        end
+      end
+
+      def has_grandson_issues?(issue)
+        return issue.descendants.visible.where(["issues.parent_id <> ?", issue.id]).count > 0
+      end
+
+    end
+  end
+end
