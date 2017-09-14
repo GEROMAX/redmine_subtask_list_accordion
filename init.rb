d15cf4481cf85a223eb1f1b14e553ce51f18bb03
GEROMAX <tobita0112@gmail.com>
2017-09-14 09:39:24 +0900

    update version number.

--- init.rb
+++ init.rb
@@ -16,7 +16,7 @@ Redmine::Plugin.register :redmine_subtask_list_accordion do
   name 'Redmine Subtask List Accordion plugin'
   author 'Ryuta Tobita'
   description 'This plugin provide accordion to subtask list of issue.'
-  version '1.3.2'
+  version '1.3.3'
   url 'https://github.com/GEROMAX/redmine_subtask_list_accordion'
   author_url 'https://github.com/GEROMAX'
 end
