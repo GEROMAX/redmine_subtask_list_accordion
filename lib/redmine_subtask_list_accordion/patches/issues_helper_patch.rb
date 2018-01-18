0870834675e8eb64afe792d692550fc4a31541dc
GEROMAX <tobita0112@gmail.com>
2018-01-18 17:52:49 +0900

    create server side function.

--- lib/redmine_subtask_list_accordion/patches/issues_helper_patch.rb
+++ lib/redmine_subtask_list_accordion/patches/issues_helper_patch.rb
@@ -23,17 +23,22 @@ module RedmineSubtaskListAccordion
 
       # add method to IssuesHelper
       def render_descendants_tree_accordion(issue)
+        trIdx = 0
         s = '<span>NEW!</span><table class="list issues odd-even">'
         issue_list(issue.descendants.visible.preload(:status, :priority, :tracker, :assigned_to).sort_by(&:lft)) do |child, level|
+          arrow = (child.descendants.visible.count > 0 ? content_tag('span', '', :class => 'treearrow') : ''.html_safe)
           css = "issue issue-#{child.id} hascontextmenu #{child.css_classes}"
+          css << " haschild" if child.children?
+          css << (expand_tree_at_first?(issue, User.current) ? " expand" : " collapse")
           css << " idnt idnt-#{level}" if level > 0
+          hide_or_show = 'display: none;' unless level <= 0 || expand_tree_at_first?(issue, User.current)
           s << content_tag('tr',
                  content_tag('td', check_box_tag("ids[]", child.id, false, :id => nil), :class => 'checkbox') +
-                 content_tag('td', link_to_issue(child, :project => (issue.project_id != child.project_id)), :class => 'subject', :style => 'width: 50%') +
+                 content_tag('td', arrow + link_to_issue(child, :project => (issue.project_id != child.project_id)), :class => 'subject', :style => 'width: 50%') +
                  content_tag('td', h(child.status), :class => 'status') +
                  content_tag('td', link_to_user(child.assigned_to), :class => 'assigned_to') +
                  content_tag('td', child.disabled_core_fields.include?('done_ratio') ? '' : progress_bar(child.done_ratio), :class=> 'done_ratio'),
-                 :class => css)
+                 :class => css, :cs => (trIdx+=1).to_s, :ce => (trIdx + child.descendants.visible.count - 1).to_s, :rank => level.to_s, :style => hide_or_show)
         end
         s << '</table>'
         s.html_safe
